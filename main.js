22526a28b9ca127ec537a3dd1b79ce6f070b784d
HUNDZVL <nguyendoanquochung9a1@gmail.com>
2023-03-15 21:06:53 +0700

    upload

--- main.js
+++ main.js
@@ -8,12 +8,33 @@ const btnChange = $("#change--page");
 const checkIcon = $(".checkp");
 const checkIcon1 = $(".checkp1");
 
+const btnheaderShow = $("#js-home");
+
+const btnclosePD = $("#js--closePD");
+
+const closePd = $("#js--closepd");
+
+const backhome = $("#js--backhome");
+
+//show product hidden home when click two btn in home
 function handleClickbtn() {
   product.classList.toggle("open");
+  btnheaderShow.classList.toggle("open")
+
 }
+
+
+
+
+// event click
+closePd.addEventListener("click", handleClickbtn);
+backhome.addEventListener("click", handleClickbtn);
+btnclosePD.addEventListener("click",handleClickbtn)
 btnChange.addEventListener("click", handleClickbtn);
 btnClick.addEventListener("click", handleClickbtn);
 
+
+//event check icon
 checkIcon.addEventListener("click", () => {
   checkIcon.classList.toggle("disablecheck");
 });
